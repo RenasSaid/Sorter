b2353e9cbf17f580ae4349d1c0a0248df25218eb
Renas Said <renas.said@live.com>
2021-11-11 13:35:48 +0100

    Added Sorting and return

--- src/Sorter/SortMain.java
+++ src/Sorter/SortMain.java
@@ -1,17 +1,17 @@
 package Sorter;
 
-import java.util.Arrays;
-
 public class SortMain {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
+
+        Sorter sorter = new Sorter();
         int [] intArgs = new int[args.length];
+
         for (int a = 0; a < args.length; a++) {
             intArgs[a] = Integer.parseInt(args[a]);
         }
-        Arrays.sort(intArgs);
-        System.out.println("Sorted list: ");
-        for (int a : intArgs) {
-            System.out.println(a);
+        int[] sorted = sorter.sort(intArgs);
+        for(int number : sorted){
+            System.out.println(number);
         }
     }
 }
--- src/Sorter/Sorter.java
+++ src/Sorter/Sorter.java
@@ -3,12 +3,8 @@ import java.util.Arrays;
 
 public class Sorter {
     public int[] sort(int[] intArgs) {
-        
-        Arrays.sort(intArgs);
-        //System.out.println("Sorted list: ");
-        //for (int a : intArgs) {
-         //   System.out.println(a);
-        //}
-        return intArgs;
+    Arrays.sort(intArgs);
+    return intArgs;
+
     }
 }
