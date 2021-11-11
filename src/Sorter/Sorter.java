e7a2ded538211c3f5200bf2d137cb0ee1574e025
Renas Said <renas.said@live.com>
2021-11-11 13:06:03 +0100

    Added Sorter to Sorter.java

--- src/Sorter/Sorter.java
+++ src/Sorter/Sorter.java
@@ -1,4 +1,14 @@
 package Sorter;
+import java.util.Arrays;
+
 public class Sorter {
-    
+    public int[] sort(int[] intArgs) {
+        
+        Arrays.sort(intArgs);
+        //System.out.println("Sorted list: ");
+        //for (int a : intArgs) {
+         //   System.out.println(a);
+        //}
+        return intArgs;
+    }
 }
