47231ac8e2c3e2cd7df0f258509ff3b47867f003
Renas Said <renas.said@live.com>
2021-11-11 11:57:27 +0100

    Added Sorter in SortMain

--- src/Sorter/SortMain.java
+++ src/Sorter/SortMain.java
@@ -1,10 +1,15 @@
 package Sorter;
+
+import java.util.Arrays;
+
 public class SortMain {
     public static void main(String[] args) throws Exception {
         int [] intArgs = new int[args.length];
         for (int a = 0; a < args.length; a++) {
             intArgs[a] = Integer.parseInt(args[a]);
         }
+        Arrays.sort(intArgs);
+        System.out.println("Sorted list: ");
         for (int a : intArgs) {
             System.out.println(a);
         }
