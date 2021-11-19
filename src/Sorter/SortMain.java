e3b581c955b70e15d1b9f351c8d79eb460474f83
Renas Said <renas.said@live.com>
2021-11-19 16:10:43 +0100

    Added Tester

--- src/Sorter/SortMain.java
+++ src/Sorter/SortMain.java
@@ -3,15 +3,15 @@ package Sorter;
 public class SortMain {
     public static void main(String[] args) {
 
-        Sorter sorter = new Sorter();
-        int [] intArgs = new int[args.length];
+        Sorter sorter = new Sorter();   //hämtar data från Sorter filen.
+        int[] intArgs = new int[args.length]; //Tar längden av argument som ges, in i listan.
 
-        for (int a = 0; a < args.length; a++) {
-            intArgs[a] = Integer.parseInt(args[a]);
+        for (int i = 0; i < args.length; i++) {  //Lopar igenom listan.
+            intArgs[i] = Integer.parseInt(args[i]);  //Parsear argument till Int.
         }
-        int[] sorted = sorter.sort(intArgs);
-        for(int number : sorted){
-            System.out.println(number);
+        int[] sorted = sorter.sort(intArgs);  //hämtar sorterad lista från Sorter
+        for(int number : sorted){       //Loopar igenom sorterad lista
+            System.out.println(number);     //Printar ut sorterad lista
         }
     }
 }
--- src/Test/SortTest.java
+++ src/Test/SortTest.java
@@ -1,6 +1,115 @@
 package Test;
+import Sorter.Sorter;
+
 public class SortTest {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
+        SortTester();
+    }
+    public static boolean SortTester(){   
+        if (testSorterMain1()){
+            System.out.println("Test1 Ok");
+        } else {
+            System.err.println("Error Test1: Not Sorted");
+        }
+
+        if (testSorterMain2()){
+            System.out.println("Test2 Ok");
+        } else {
+            System.err.println("Error Test2: Not Sorted");
+        }
+
+        if (testSorterMain3()){
+            System.out.println("Test3 Ok");
+        } else {
+            System.err.println("Error Test3: Not Sorted");
+        }
+
+        if (testSorterMain4()){
+            System.out.println("Test4 Ok");
+        } else {
+            System.err.println("Error Test4: Not Sorted");
+        }
+
+        if (testSorterMain5()){
+            System.out.println("Test5 Ok");
+        } else {
+            System.err.println("Error Test5: Not Sorted");
+        }
         
+        return true;
+    }
+
+    public static boolean testSorterMain1(){
+        Sorter s = new Sorter();
+        int[] arrList = {337, 765, 525, 234};
+        int[] result = s.sort(arrList);
+        int[] expected = {6, 234, 337, 525, 765};
+        for (int i = 0; i < result.length; i++) {
+            if (result[i] != expected[i]){
+                System.out.println("Test1 Failed");
+                return false;
+            }
+            
+        }
+        return true;
+    }
+    public static boolean testSorterMain2(){
+        Sorter s = new Sorter();
+        int[] arrList = {};
+        int[] result = s.sort(arrList);
+        int[] expected = {};
+        for (int i = 0; i < result.length; i++) {
+            if (result[i] != expected[i]){
+                System.out.println("Test2 Failed");
+                return false;
+            }
+            
+        }
+        return true;
+    }
+
+    public static boolean testSorterMain3(){
+        Sorter s = new Sorter();
+        int[] arrList = {-1, 23, -55, -134, 0};
+        int[] result = s.sort(arrList);
+        int[] expected = {-134, -55, -1, 0, 23};
+        for (int i = 0; i < result.length; i++) {
+            if (result[i] != expected[i]){
+                System.out.println("Test3 Failed");
+                return false;
+            }
+            
+        }
+        return true;
+    }
+
+    public static boolean testSorterMain4(){
+        Sorter s = new Sorter();
+        int[] arrList = {6, 337, 765, 525, 234};
+        int[] result = s.sort(arrList);
+        int[] expected = {6, 234, 337, 525, 765};
+        for (int i = 0; i < result.length; i++) {
+            if (result[i] != expected[i]){
+                System.out.println("Test4 Failed");
+                return false;
+            }
+            
+        }
+        return true;
+    }
+
+    public static boolean testSorterMain5(){
+        Sorter s = new Sorter();
+        int[] arrList = {6, 337, 765, 525, 234};
+        int[] result = s.sort(arrList);
+        int[] expected = {6, 234, 337, 525, 765};
+        for (int i = 0; i < result.length; i++) {
+            if (result[i] != expected[i]){
+                System.out.println("Test5 Failed");
+                return false;
+            }
+            
+        }
+        return true;
     }
 }
